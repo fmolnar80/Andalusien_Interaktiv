8e0446acc9cd827db4e2692a14b3ca2bbbffabad
fmolnar80 <fmolnar@me.com>
2026-08-06 23:03:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE = 'andalusien-2026-v20';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+self.addEventListener('fetch', function(e){
+  var req = e.request;
+  if (req.method !== 'GET') return;
+  var accept = req.headers.get('accept') || '';
+  var isHTML = req.mode === 'navigate' || accept.indexOf('text/html') !== -1;
+  if (isHTML) {
+    // ONLINE immer frisch laden, offline aus Cache
+    e.respondWith(
+      fetch(req).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put('./index.html', copy); });
+        return res;
+      }).catch(function(){ return caches.match('./index.html'); })
+    );
+  } else {
+    // statische Dateien: erst Cache, sonst Netz
+    e.respondWith(
+      caches.match(req).then(function(cached){
+        return cached || fetch(req).then(function(res){
+          var copy = res.clone();
+          caches.open(CACHE).then(function(c){ c.put(req, copy); });
+          return res;
+        });
+      })
+    );
+  }
+});
